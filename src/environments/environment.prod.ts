810517435278aec6cdcb7f703bbe7e7a4bf73318
manikandan.siamgtp <manikandan.siamgtp@gmail.com>
2022-07-20 10:56:11 +0530

    api changes completed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apirootURL: 'http://apisop.underdev.in:9001/api/v1/web/repository-list/',
+  apirootURL: 'http://apisop.underdev.in:9001/api/v1/web/',
 };
