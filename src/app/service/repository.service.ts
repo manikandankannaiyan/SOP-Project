8442fb03e04dd769104c7edd880afc40c811c3c8
manikandan.siamgtp <manikandan.siamgtp@gmail.com>
2022-07-20 15:14:17 +0530

    api header change

--- src/app/service/repository.service.ts
+++ src/app/service/repository.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -8,9 +8,14 @@ import { environment } from 'src/environments/environment';
 })
 export class RepositoryService {
 
+  httpHeader={
+    headers:new HttpHeaders({
+      'Authorization':'Token 0aeb15de83ef6fb02695f2ce0d99374046f32eb9'
+    })
+  }
   constructor(private http: HttpClient) { }
 
   getdata(data:any):Observable<any>{
-    return this.http.post<any>(environment.apirootURL+'repository-list/', data,{headers:{'Authorization':'Token 0aeb15de83ef6fb02695f2ce0d99374046f32eb9'}});
+    return this.http.post<any>(environment.apirootURL+'repository-list/', data,this.httpHeader);
   }
 }
